53330ba8be37a83385b525962456a21ff802acd7
DaniV <dany.17.com@hotmail.com>
2024-09-18 10:39:07 -0600

    Detalle con zl y zh

--- FigurasMatriz8x8/Matriz8x8/main.asm
+++ FigurasMatriz8x8/Matriz8x8/main.asm
@@ -5,17 +5,20 @@
 	.def cont1=r17
 	.def cont2=r18
 	.def cont3=r19
+	.def temp2=r20
+	.def tempx=r21
+	.def tempy=r22
 	.cseg
 	.org $0
 
-	ldi temp, $7F
-	out ddrc, temp ;Configuramos solo C7 como entrada, los demas son salidas
+	ldi temp, $0F
+	out ddrc, temp ;Configuramos solo C5 como entrada, los demas son salidas
 
-	ldi temp, $80
-	out portc, temp ;Habilitamos resistencias de pull-up en c7
+	ldi temp, $20
+	out portc, temp ;Habilitamos resistencias de pull-up en c5
 
 	;Habilitamos salidas faltantes
-	ldi temp, $FF
+	ldi temp, $0F
 	out ddrb, temp
 
 	ldi temp, $FF
@@ -23,84 +26,63 @@
 
 main:
 	in temp, pinc
-	andi temp, $80 ;Si se presiona c7 muestra una figura diferente
+	andi temp, $20 ;Si se presiona c5 muestra una figura diferente
 	breq figura2
 
-	;Sino que se mantenga mostrando una figura
 	ldi zl,low(figura1_data*2)
 	ldi zh, high(figura1_data*2)
-
-	ldi temp, $08
-	out portb, temp ;Enciendo fila 1 (b3)
-	call encenderCol
-	ldi temp, $00
-	out portb, temp ;Apago fila
-
-	ldi temp, $04
-	out portb, temp ;Enciendo fila 1 (b2)
-	call encenderCol
-	ldi temp, $00
-	out portb, temp ;Apago fila
-
-	ldi temp, $02
-	out portb, temp ;Enciendo fila 1 (b1)
-	call encenderCol
-	ldi temp, $00
-	out portb, temp ;Apago fila
-
 	ldi temp, $01
-	out portb, temp ;Enciendo fila 1 (b0)
-	call encenderCol
-	ldi temp, $00
-	out portb, temp ;Apago fila
+	jmp encenderMatriz
 
-	ldi temp, $08
-	out portc, temp ;Enciendo fila 1 (c3)
-	call encenderCol
-	ldi temp, $00
-	out portc, temp ;Apago fila
-
-	ldi temp, $04
-	out portc, temp ;Enciendo fila 1 (c2)
-	call encenderCol
-	ldi temp, $00
-	out portc, temp ;Apago fila
-
-	ldi temp, $02
-	out portc, temp ;Enciendo fila 1 (c1)
-	call encenderCol
-	ldi temp, $00
-	out portc, temp ;Apago fila
-
+figura2:
+	ldi zl,low(figura2_data*2)
+	ldi zh, high(figura2_data*2)
 	ldi temp, $01
-	out portc, temp ;Enciendo fila 1 (c0)
-	call encenderCol
+
+encenderMatriz:
+
+	mov temp2, temp
+	com temp
+	out portd, temp ;Enciendo cada columna (Puerto D)
+
+	call encenderFil ;Enciendo fila
 	ldi temp, $00
-	out portc, temp ;Apago fila
+	out portd, temp ;Apago columna
 
-	jmp main ;Vuelvo a leer entrada
+	mov temp, temp2
+	lsl temp
 
-encenderCol:
-	lpm temp, z+
-	out portd, temp
+	cpi temp, $80
+	breq main
+
+	jmp encenderMatriz
+
+encenderFil:
+	lpm xl, zl
+	lpm xh, zh
+	inc z
+
+	out portb, xl
+	out portc, xh
 	call delay125m
 	ret
 
 figura1_data: .db $3E, $41, $92, $84, $88, $86, $81, $3E
-
-figura2:
-	ret
+figura2_data: .db $81, $18, $3C, $7E, $72, $52, $5E, $81
 
 delay125m:
-	ldi cont3, 5
+	ldi cont3, 10
 
 lazo3:
-	ldi cont2, 200
+	ldi cont2, 10
 
 lazo2:
-	ldi cont1, 200
+	ldi cont1, 16
 
 lazo1:
+	nop
+	nop
+	nop
 	nop
 	nop
 	nop
