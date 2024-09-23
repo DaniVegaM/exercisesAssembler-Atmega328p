4cf1831c280170cbaac0ba54149270043796aa3e
0ambar <abrahamr677@gmail.com>
2024-09-23 11:14:55 -0600

    commit

--- FigurasMatriz8x8/Matriz8x8/main.asm
+++ FigurasMatriz8x8/Matriz8x8/main.asm
@@ -5,20 +5,25 @@
 	.def cont1=r17
 	.def cont2=r18
 	.def cont3=r19
-	.def temp2=r20
-	.def tempx=r21
-	.def tempy=r22
 	.cseg
 	.org $0
 
+<<<<<<< Updated upstream
 	ldi temp, $0F
 	out ddrc, temp ;Usaremos C5 como entrada, los demas son salidas
 
 	ldi temp, $F0
 	out portc, temp ;Habilitamos resistencias de pull-up en c5
+=======
+	ldi temp, $7F
+	out ddrc, temp ;Configuramos solo C7 como entrada, los demas son salidas
+
+	ldi temp, $80
+	out portc, temp ;Habilitamos resistencias de pull-up en c7
+>>>>>>> Stashed changes
 
 	;Habilitamos salidas faltantes
-	ldi temp, $0F
+	ldi temp, $FF
 	out ddrb, temp
 
 	ldi temp, $FF
@@ -26,11 +31,13 @@
 
 main:
 	in temp, pinc
-	andi temp, $20 ;Si se presiona c5 muestra una figura diferente
+	andi temp, $80 ;Si se presiona c7 muestra una figura diferente
 	breq figura2
 
+	;Sino que se mantenga mostrando una figura
 	ldi zl,low(figura1_data*2)
 	ldi zh, high(figura1_data*2)
+<<<<<<< Updated upstream
 	ldi temp, $01
 	jmp filasC
 
@@ -48,10 +55,22 @@ filasC:
 	call encenderCol ;Enciendo columna
 	ldi temp, $0F
 	out portc, temp ;Apago fila
+=======
 
-	mov temp, temp2
-	lsl temp
+	ldi temp, $08
+	out portb, temp ;Enciendo fila 1 (b3)
+	call encenderCol
+	ldi temp, $00
+	out portb, temp ;Apago fila
+>>>>>>> Stashed changes
 
+	ldi temp, $04
+	out portb, temp ;Enciendo fila 1 (b2)
+	call encenderCol
+	ldi temp, $00
+	out portb, temp ;Apago fila
+
+<<<<<<< Updated upstream
 	cpi temp, $10 ;Termino con la fila de los c ahora van los b
 	breq filasB_prep
 
@@ -79,27 +98,73 @@ filasB:
 encenderCol:
 	lpm temp, z+
 
+=======
+	ldi temp, $02
+	out portb, temp ;Enciendo fila 1 (b1)
+	call encenderCol
+	ldi temp, $00
+	out portb, temp ;Apago fila
+
+	ldi temp, $01
+	out portb, temp ;Enciendo fila 1 (b0)
+	call encenderCol
+	ldi temp, $00
+	out portb, temp ;Apago fila
+
+	ldi temp, $08
+	out portc, temp ;Enciendo fila 1 (c3)
+	call encenderCol
+	ldi temp, $00
+	out portc, temp ;Apago fila
+
+	ldi temp, $04
+	out portc, temp ;Enciendo fila 1 (c2)
+	call encenderCol
+	ldi temp, $00
+	out portc, temp ;Apago fila
+
+	ldi temp, $02
+	out portc, temp ;Enciendo fila 1 (c1)
+	call encenderCol
+	ldi temp, $00
+	out portc, temp ;Apago fila
+
+	ldi temp, $01
+	out portc, temp ;Enciendo fila 1 (c0)
+	call encenderCol
+	ldi temp, $00
+	out portc, temp ;Apago fila
+
+	jmp main ;Vuelvo a leer entrada
+
+encenderCol:
+	lpm temp, z+
+>>>>>>> Stashed changes
 	out portd, temp
 	call delay125m
 	ret
 
+<<<<<<< Updated upstream
 ;Datos para las columnas que prenden con 1
 figura1_data: .db $3E, $41, $92, $84, $88, $86, $81, $7E
 figura2_data: .db $81, $18, $3C, $7E, $72, $52, $5E, $81
+=======
+figura1_data: .db $3E, $41, $92, $84, $88, $86, $81, $3E
+
+figura2:
+	ret
+>>>>>>> Stashed changes
 
 delay125m:
-	ldi cont3, 10
+	ldi cont3, 100
 
 lazo3:
-	ldi cont2, 10
+	ldi cont2, 100
 
 lazo2:
-	ldi cont1, 16
+	ldi cont1, 160
 
 lazo1:
-	nop
-	nop
-	nop
 	nop
 	nop
 	nop
