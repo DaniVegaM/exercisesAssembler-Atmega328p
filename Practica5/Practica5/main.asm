566a28e9bb657f0707c36a9b83230e25d529a1ba
0ambar <abrahamr677@gmail.com>
2024-11-14 09:55:18 -0600

    practica 6 final

--- Practica5/Practica5/main.asm
+++ Practica5/Practica5/main.asm
@@ -11,7 +11,7 @@
 	.def uni_aux = r23
 	.def dec_aux = r24
 	.def cen_aux = r25
-	.def temp4 = r1
+	.def temp4 = r2
 	.cseg 
 
 	.org 0
@@ -28,6 +28,7 @@
 	.org $020
 	jmp timer0_ovf
 
+
 reset:
 	// CONFIGURACION DE PUERTOS
 	ldi temp, $FB ;1111 1011
@@ -43,30 +44,33 @@ reset:
 	out PORTB, temp ; Habilitamos PULL-UP en B0 y B1 
 
 	ldi temp, $07 ;0000 0111
-	out DDRC, temp; Configuramos C0, C1, C2 como salidas	
+	out DDRC, temp; Configuramos C0, C1, C2 como salidas		
 
 	
 	// CONFIGURACION DE TIMERS E INTERRUPCIONES
+	// TIMER0_OVF
 	ldi temp, $03
 	out tccr0b, temp ;seleccionamos modo normal y prescaler = 64
 	
 	ldi temp, $01
 	sts timsk0, temp ;habilitamos interrupcion por sobreflujo
 
+	// INT0 POR FLANCOS DE SUBIDA
 	ldi temp, $03
 	sts EICRA, temp ;Configuramos la deteccion de flancos de subida 
 
 	ldi temp, $01
 	out EIMSK, temp ;Habilitamos el INT0 (interrupcion externa 0)
-		
-	ldi temp, $01
-	sts TIMSK2, temp
 
+	// TIMER2_OVF		
+	ldi temp, $01 
+	sts TIMSK2, temp ;Configuramos timer2 por sobreflujo
+
+	//TIMER1_COMPA
 	ldi temp, $0C ;Prescaler de 256 y modo de comparacion
 	sts TCCR1B, temp 
 
-	; VALOR DE COMPARACION = 62500 = $F424
-	ldi temp, $F4
+	ldi temp, $F4 ; VALOR DE COMPARACION = 62500 = $F424
 	sts OCR1AH, temp ;Primero escribimos la parte alta del comparacion
 
 	ldi temp, $24
@@ -74,8 +78,6 @@ reset:
 
 	ldi temp, $02
 	sts TIMSK1, temp ;Habilitamos modo de comparacion 
-
-	sei
 
 
 	// INICIALIZACION DE REGISTROS
@@ -87,22 +89,21 @@ reset:
 	ldi cen_aux, 0
 	ldi mux, $01 ;PARA ANODO COMUN
 
-	sei
-
-
 	// CARGA EN MEM DE PROGRAMA
 	ldi ZL, low(display7s*2)
 	ldi ZH, high(display7s*2)
-	ldi XL, $00
 	ldi XH, $01
+	ldi XL, $00
 
-	ldi temp, $0B
+	ldi temp, $0C
 
 lazo: 
 	lpm temp2, z+ ;Carga en memoria de programa cada valor para el display
 	st x+, temp2 ;Almacena los valores en memoria de datos
 	dec temp
 	brne lazo
+	
+	sei
 
 main:	
 	in temp, PINB
@@ -116,75 +117,6 @@ main:
 	breq f_64
 
 	jmp f_32
-
-
-int_0: 
-	inc unidad
-	cpi unidad, $0A
-	breq reset_unidad
-	reti
-
-reset_unidad: 
-	ldi unidad, $00
-	inc decena
-	cpi decena, $0A
-	breq reset_decena
-	reti
-
-reset_decena:
-	ldi decena, $00
-	inc centena
-	cpi centena, $0A
-	breq rayita
-	reti
-
-rayita:
-	ldi unidad, $0A
-	ldi decena, $0A
-	ldi centena, $0A
-	reti
-
-
-// MULTIPLEXEO DE LOS DISPLAYS CATODO COMUN
-timer0_ovf:
-	ldi temp3, $00
-	out PORTC, temp3 ;Apaga displays
-
-	cpi mux, $02
-	breq mux_decena ;Caso para decena
-
-	cpi mux, $04
-	brne mux_centena
-
-	; Caso para unidad (mux_undidad)
-	mov r26, uni_aux
-	ld temp3, x
-	in temp4, ocr0a
-	and temp3, temp4
-	out PORTD, temp3
-	out PORTC, mux
-	ldi mux, $02
-	reti
-
-mux_decena: 
-	mov r26, dec_aux
-	ld temp3, x
-	in temp4, ocr0a
-	and temp3, temp4
-	out PORTD, temp3
-	out PORTC, mux
-	ldi mux, $04
-	reti
-
-mux_centena: 
-	mov r26, cen_aux
-	ld temp3, x
-	in temp4, ocr0a
-	and temp3, temp4
-	out PORTD, temp3
-	out PORTC, mux
-	ldi mux, $01
-	reti
 
 
 // CAMBIO DE PRESCALER para TCCR2B
@@ -209,6 +141,80 @@ f_32:
 	jmp main
 
 
+// INTERRUPCION EXTERNA
+int_0: 
+	inc unidad
+	cpi unidad, $0A
+	breq reset_unidad
+	sbi EIFR, 0
+	reti
+
+reset_unidad: 
+	ldi unidad, $00
+	inc decena
+	cpi decena, $0A
+	breq reset_decena
+	sbi EIFR, 0
+	reti
+
+reset_decena:
+	ldi decena, $00
+	inc centena
+	cpi centena, $0A
+	brge error
+	sbi EIFR, 0
+	reti
+
+error:
+	ldi unidad, $0B
+	ldi decena, $0B
+	ldi centena, $0A
+	sbi EIFR, 0
+	reti
+
+
+// MULTIPLEXEO DE LOS DISPLAYS CATODO COMUN
+timer0_ovf:
+	ldi temp3, $00
+	out PORTC, temp3 ;Apaga displays
+
+	cpi mux, $02
+	breq mux_decena ;Caso para decena
+
+	cpi mux, $04
+	breq mux_centena
+
+	; Caso para unidad (mux_undidad)
+	mov r26, uni_aux
+	ld temp3, x
+	sbis PIND, 2
+	cbi PIND, 2
+	out PORTD, temp3
+	out PORTC, mux
+	ldi mux, $02
+	reti
+
+mux_decena: 
+	mov r26, dec_aux
+	ld temp3, x
+	sbis PIND, 2
+	cbi PIND, 2
+	out PORTD, temp3
+	out PORTC, mux
+	ldi mux, $04
+	reti
+
+mux_centena: 
+	mov r26, cen_aux
+	ld temp3, x
+	sbis PIND, 2
+	cbi PIND, 2
+	out PORTD, temp3
+	out PORTC, mux
+	ldi mux, $01
+	reti
+
+
 // REALIZA EL TOGGLE PARA LAS FRECUENCIAS
 timer2_ovf:
 	in temp2, PINB
@@ -228,10 +234,11 @@ timer1_COMPA:
 	ldi unidad, 0
 	ldi decena, 0
 	ldi centena, 0
+	reti
 
 display7s:
-	;.db $03, $9F, $25, $0D, $99, $49, $41, $1F, $01, $09,  ;Valores para display7s anodo comun
-	.db $80, $F1, $48, $60, $31, $22, $02, $70, $00, $20, $7B, $7B
-	;     0,   1,   2,   3,   4,   5,   6,   7,   8,   9, raya
+	.db $80, $F1, $48, $60, $31, $22, $02, $F0, $00, $20, $0A, $5B
+	;     0,   1,   2,   3,   4,   5,   6,   7,   8,   9,   E,  r
+	;                                                       0A, 0B
 
 
